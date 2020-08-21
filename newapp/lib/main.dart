9dcc22f3896265d535245dd61027852bfbbfe947
Oswaldo Rodriguez <OwnOS@oswaldos-mbp.home>
2020-08-21 16:44:43 -0400

    Cards Page

--- newapp/lib/main.dart
+++ newapp/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:newapp/src/pages/alert_page.dart';
-import 'package:newapp/src/pages/avatar_page.dart';
-
-import 'package:newapp/src/pages/home_page.dart';
+import 'package:newapp/src/routes/routes.dart';
 // import 'package:other_app/src/pages/home_temp.dart';
 
 void main() => runApp(MyApp());
@@ -14,12 +12,7 @@ class MyApp extends StatelessWidget {
       title: 'Components App',
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => HomePage(),
-        'alert': (BuildContext context) => AlertPage(),
-        'avatar': (BuildContext context) => AvatarPage(),
-        // 'alert'
-      },
+      routes: getApplicationRoutes(),
       onGenerateRoute: (RouteSettings settings) {
         print('Ruta llamada ${settings.name}');
         return MaterialPageRoute(
