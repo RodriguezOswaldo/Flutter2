3f151b800ff49047aba63936433290212319e200
Oswaldo Rodriguez <owner2303@gmail.com>
2020-09-15 11:08:23 -0400

    route

--- newapp/lib/src/routes/routes.dart
+++ newapp/lib/src/routes/routes.dart
@@ -15,7 +15,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'card': (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
     'inputs': (BuildContext context) => InputPage(),
-    'list': (BuildContext context) => SliderPage(),
+    'slider': (BuildContext context) => SliderPage(),
     // 'alert'
   };
 }
