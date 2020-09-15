d736af61ae07ce437c477ffab4b6b04611bd5b6f
Oswaldo Rodriguez <owner2303@gmail.com>
2020-09-14 23:53:52 -0400

    localizations package

--- newapp/lib/main.dart
+++ newapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:newapp/src/pages/alert_page.dart';
 import 'package:newapp/src/routes/routes.dart';
 // import 'package:other_app/src/pages/home_temp.dart';
@@ -9,6 +10,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //Change Language! make sure the dependencies has been exported
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'),
+        const Locale('es', 'ES'),
+      ],
       title: 'Components App',
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
