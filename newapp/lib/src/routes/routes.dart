f398fa7a9a2a3e96471a48628fb1ad878b677dff
Oswaldo Rodriguez <owner2303@gmail.com>
2020-09-15 14:10:47 -0400

    list view file

--- newapp/lib/src/routes/routes.dart
+++ newapp/lib/src/routes/routes.dart
@@ -5,6 +5,7 @@ import 'package:newapp/src/pages/avatar_page.dart';
 import 'package:newapp/src/pages/card_page.dart';
 import 'package:newapp/src/pages/home_page.dart';
 import 'package:newapp/src/pages/input_page.dart';
+import 'package:newapp/src/pages/listview_page.dart';
 import 'package:newapp/src/pages/slider_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
@@ -16,6 +17,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
     'inputs': (BuildContext context) => InputPage(),
     'slider': (BuildContext context) => SliderPage(),
+    'list': (BuildContext context) => ListPage(),
     // 'alert'
   };
 }
