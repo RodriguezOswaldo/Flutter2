2b1bcfa0a69540abf5aad4c55c898ba94c7a3bef
Oswaldo Rodriguez <owner2303@gmail.com>
2020-09-15 10:49:03 -0400

    slider page into route

--- newapp/lib/src/routes/routes.dart
+++ newapp/lib/src/routes/routes.dart
@@ -5,6 +5,7 @@ import 'package:newapp/src/pages/avatar_page.dart';
 import 'package:newapp/src/pages/card_page.dart';
 import 'package:newapp/src/pages/home_page.dart';
 import 'package:newapp/src/pages/input_page.dart';
+import 'package:newapp/src/pages/slider_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
@@ -14,6 +15,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'card': (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
     'inputs': (BuildContext context) => InputPage(),
+    'list': (BuildContext context) => SliderPage(),
     // 'alert'
   };
 }
