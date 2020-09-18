bd7ccb614a6bd11e573d7577f56874697d4e4754
Oswaldo Rodriguez <owner2303@gmail.com>
2020-09-18 18:47:22 -0400

    adding lists

--- newapp/lib/src/pages/listview_page.dart
+++ newapp/lib/src/pages/listview_page.dart
@@ -6,6 +6,20 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
+  ScrollController _scrollController = new ScrollController();
+
+  List<int> _numberList = new List();
+  int _lastItem = 0;
+  void initState() {
+    super.initState();
+    _add10();
+    _scrollController.addListener(
+      () {
+        print('Scroll!!');
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +27,30 @@ class _ListPageState extends State<ListPage> {
         title: Text('List Views'),
         backgroundColor: Color.fromRGBO(433, 130, 12, 0.9),
       ),
-      body: Text('Listas....'),
+      body: _createList(),
     );
+  }
+
+  Widget _createList() {
+    return ListView.builder(
+      controller: _scrollController,
+      itemCount: _numberList.length,
+      itemBuilder: (BuildContext context, int index) {
+        final image = _numberList = [index];
+        return FadeInImage(
+          image:
+              NetworkImage('https://source.unsplash.com/random/500x30$image'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+        );
+      },
+    );
+  }
+
+  void _add10() {
+    for (var i = 1; i < 10; i++) {
+      _lastItem++;
+      _numberList.add(_lastItem);
+    }
+    setState(() {});
   }
 }
