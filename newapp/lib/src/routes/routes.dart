d5d917a977961f8ab243bcd8b3fe0d95f17a42b4
Oswaldo Rodriguez <owner2303@gmail.com>
2020-09-18 20:33:22 -0400

    trying to fix icons

--- newapp/lib/src/routes/routes.dart
+++ newapp/lib/src/routes/routes.dart
@@ -8,11 +8,13 @@ import 'package:newapp/src/pages/input_page.dart';
 import 'package:newapp/src/pages/listview_page.dart';
 import 'package:newapp/src/pages/slider_page.dart';
 
+import '../pages/avatar_page.dart';
+
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
     '/': (BuildContext context) => HomePage(),
     'alert': (BuildContext context) => AlertPage(),
-    'avatar': (BuildContext context) => AvatarPage(),
+    AvatarPage.pageName: (BuildContext context) => AvatarPage(),
     'card': (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
     'inputs': (BuildContext context) => InputPage(),
