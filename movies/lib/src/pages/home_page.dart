c1ec4eb7426f87f47261d71fdfd901ef14b5b4fc
Oswaldo Rodriguez <owner2303@gmail.com>
2020-09-21 22:08:42 -0400

    add swiper

--- movies/lib/src/pages/home_page.dart
+++ movies/lib/src/pages/home_page.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Text('Hola Mundo!!!!!!'),
-    ));
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text('Movies in Teathre '),
+          backgroundColor: Colors.redAccent,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              _swiperCard(),
+            ],
+          ),
+        ));
+  }
+
+  Widget _swiperCard() {
+    return Swiper(
+      itemBuilder: (BuildContext context, int Index) {
+        return new Image.network("http://via.placeholder.com/350x150",
+            fit: BoxFit.fill);
+      },
+      itemCount: 3,
+      pagination: new SwiperPagination(),
+      control: new SwiperControl(),
+    );
   }
 }
